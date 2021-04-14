60b0ce259ae260308732816131bb55f404b2dd78
Ahmed818 <AhmedSobhyErfan@gmail.com>
2021-04-14 23:36:57 +0200

    add country

--- Core/Entities/Address.cs
+++ Core/Entities/Address.cs
@@ -5,5 +5,6 @@
         public string Street { get; set; }
         public int Number    { get; set; }
         public string City   { get; set; }
+        public string Country { get; set; }
     }
 }
